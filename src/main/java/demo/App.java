9fee337d6c1f107645028eeabb8037df038c6127
omkarbhagwatwar@gmail.com <omkarbhagwatwar@gmail.com>
2024-11-12 12:36:38 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,21 +1,23 @@
 package demo;
-import java.net.MalformedURLException;
 
+import java.net.MalformedURLException;
 
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
-        
+
         // This is to remove unnecessary warnings from your console
         System.setProperty("java.util.logging.config.file", "logging.properties");
-        
+
         TestCases tests = new TestCases(); // Initialize your test class
 
-        //TODO: call your test case functions one after other here
+        // TODO: call your test case functions one after other here
 
         tests.testCase01();
+        tests.testCase02();
+        tests.testCase03();
+        tests.testCase04();
 
-        //END Tests
-
+        // END Tests
 
         tests.endTest(); // End your test by clearning connections and closing browser
     }
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,7 +1,9 @@
 package demo;
 
-import java.util.concurrent.TimeUnit;
-
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,11 +12,10 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
-    {
+
+    public TestCases() {
         System.out.println("Constructor: TestCases");
 
         WebDriverManager.chromedriver().timeout(30).setup();
@@ -33,24 +34,112 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    public void endTest()
-    {
+    public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
         driver.quit();
-
     }
 
-    
-    public  void testCase01(){
+    public void testCase01() {
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        boolean status = false;
+
+        // Navigate to Leetcode homepage
+        driver.get("https://leetcode.com/");
+
+        // Verify that the URL contains "leetcode"
+        String url = driver.getCurrentUrl();
+        if (url.contains("leetcode")) {
+            status = true;
+            System.out.println("URL contains 'leetcode': Test Passed");
+        } else {
+            System.out.println("URL does not contain 'leetcode': Test Failed");
+        }
+
+        System.out.println("End Test case: testCase01");
     }
 
+    public void testCase02() {
+        System.out.println("Start Test case: testCase02");
+        boolean status = false;
 
+        // Click on the "Questions" link to navigate to the problem set page
+        WebElement questionsLink = driver.findElement(By.xpath("//p[text()='View Questions ']"));
+        questionsLink.click();
+
+        // Verify that the URL contains "problemset"
+        String url = driver.getCurrentUrl();
+        if (url.contains("https://leetcode.com/problemset/")) {
+            status = true;
+            System.out.println("URL contains 'problemset': Test Passed");
+        } else {
+            System.out.println("URL does not contain 'problemset': Test Failed");
+        }
+
+        // Retrieve and print details of the first 5 questions
+        List<WebElement> questions = driver.findElements(By.xpath("//a[text()='Two Sum']")); 
+        for (int i = 0; i < 5 && i < questions.size(); i++) {
+            String questionTitle = questions.get(i).getText();
+            System.out.println("Question " + (i + 1) + ": " + questionTitle);
+
+            // Verify the title of each question
+            questions.get(i).click();
+            String titleOnPage = driver.getTitle();
+            if (titleOnPage.contains(questionTitle)) {
+                System.out.println("Title matches for question: " + questionTitle);
+                driver.navigate().back();
+            } else {
+                System.out.println("Title does not match for question: " + questionTitle);
+            }
+        }
+
+        System.out.println("End Test case: testCase02");
+    }
+
+    public void testCase03() {
+        System.out.println("Start Test case: testCase03");
+        boolean status = false;
+
+        // Open the Two Sum problem
+        driver.get("https://leetcode.com/problems/two-sum/description/");
+
+        // Get the current URL
+        String currentUrl = driver.getCurrentUrl();
+
+        // Verify that the URL contains "two-sum"
+        if (currentUrl.contains("https://leetcode.com/problems/two-sum/description/")) {
+            status = true;
+            System.out.println("URL contains 'two-sum': Test Passed");
+        } else {
+            System.out.println("URL does not contain 'two-sum': Test Failed");
+        }
+
+        System.out.println("End Test case: testCase03");
+    }
+
+    public void testCase04() {
+        System.out.println("Start Test case: testCase04");
+        boolean status = false;
+
+        // Open the Two Sum problem page first
+        driver.get("https://leetcode.com/problems/two-sum/");
+
+        // Click on the Submissions tab
+        WebElement submissionsTab = driver.findElement(By.xpath("//span[text()='Submit']"));
+        submissionsTab.click();
+
+        // Verify if the "Register or Sign In" message is displayed
+        WebElement messageElement = driver.findElement(By.xpath("//a[text()='Login / Sign up']"));
+        if (messageElement.isDisplayed()) {
+            status = true;
+            System.out.println("'Register or Sign In' is displayed: Test Passed");
+        } else {
+            System.out.println("'Register or Sign In' is not displayed: Test Failed");
+        }
+
+        System.out.println("End Test case: testCase04");
+    }
 }
