21db44825d3e9b92d2aebe619d7bb178dcac222a
omkarbhagwatwar@gmail.com <omkarbhagwatwar@gmail.com>
2024-11-13 17:12:15 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -11,14 +11,22 @@ public class App {
         TestCases tests = new TestCases(); // Initialize your test class
 
         // TODO: call your test case functions one after other here
+        int count = 0;
 
-        tests.testCase01();
-        tests.testCase02();
-        tests.testCase03();
-        tests.testCase04();
-
+        if (tests.testCase01()) {
+            count++;
+        }
+        if (tests.testCase02()) {
+            count++;
+        }
+        if (tests.testCase03()) {
+            count++;
+        }
+        if (tests.testCase04()) {
+            count++;
+        }
+        System.out.println("Total no of testcases " + count);
         // END Tests
-
         tests.endTest(); // End your test by clearning connections and closing browser
     }
 
@@ -26,3 +34,37 @@ public class App {
         new App().getGreeting();
     }
 }
+
+// package demo;
+
+// import java.net.MalformedURLException;
+
+// public class App {
+// public void getGreeting() throws InterruptedException, MalformedURLException
+// {
+
+// // This is to remove unnecessary warnings from your console
+// System.setProperty("java.util.logging.config.file", "logging.properties");
+
+// TestCases tests = new TestCases(); // Initialize your test class
+
+// // TODO: call your test case functions one after other here
+
+// tests.testCase01();
+// tests.testCase02();
+// tests.testCase03();
+// tests.testCase04();
+
+// // END Tests
+
+// tests.endTest(); // End your test by clearning connections and closing
+// browser
+// }
+
+// public static void main(String[] args) throws InterruptedException,
+// MalformedURLException {
+// new App().getGreeting();
+// }
+// }
+
+// // omkarbhagwatwar-ME_QA_XLEETCODE1
